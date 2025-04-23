68ba8556d40f6b245bb12905696435161bc20766
lindster88 <lindseyclaireleong@gmail.com>
2025-04-23 14:56:36 -0700

    whiteboard
    it's broken!!!

---
+++ Assets/Whiteboard/pen_script.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class Colors : MonoBehaviour
+{
+    [SerializeField] public Color32 myColor;
+    [SerializeField] private Whiteboard whiteboard_script;
+    [SerializeField] public Vector2 pen_dims = new Vector2(10,  10);
+    public Color[] myColorArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myColor = Color.black;
+        set_pen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void set_pen()
+    {
+        myColorArray = new Color[(int)pen_dims.x * (int)pen_dims.y];
+        for (int i = 0; i < myColorArray.Length; ++i)
+        {
+            myColorArray[i] = myColor;
+        }
+    }
+
+    // there's definitely a better way to do this
+    public void set_black()
+    {
+        myColor = Color.black;
+        set_pen();
+    }
+    public void set_white()
+    {
+        myColor = Color.white;
+        set_pen();
+    }
+    public void set_red()
+    {
+        myColor = Color.red;
+        set_pen();
+    }
+    public void set_orange()
+    {
+        myColor = new Color32(249, 105, 14, 1);
+        set_pen();
+    }
+    public void set_yellow()
+    {
+        myColor = Color.yellow;
+        set_pen();
+    }
+    public void set_green()
+    {
+        myColor = Color.green;
+        set_pen();
+    }
+    public void set_blue()
+    {
+        myColor = Color.blue;
+        set_pen();
+    }
+    public void set_purple()
+    {
+        myColor = new Color32(90, 34, 139, 1);
+        set_pen();
+    }
+    public void set_eraser()
+    {
+        // change adaptive to bg later
+        myColor = Color.white;
+        set_pen();
+    }
+}
---
+++ Assets/Whiteboard/whiteboard_script.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditorInternal.ReorderableList;
+using Color = UnityEngine.Color;
+
+public class Whiteboard : MonoBehaviour
+{
+    public Texture2D texture;
+    public Vector2 textureSize;
+    public bool whiteboardHover = false;
+    [SerializeField] private int penSize;
+    [SerializeField] private Colors pen_script;
+    private Renderer whiteboard;
+    private bool pressingMouse;
+
+    Vector3 mousePositionOffset;
+    public float defaultX;
+    public float defaultY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteboard = GetComponent<Renderer>();
+        textureSize = new Vector2(x: whiteboard.bounds.size.x, y: whiteboard.bounds.size.y);
+        texture = new Texture2D(width: (int)textureSize.x, height: (int)textureSize.y);
+        whiteboard.material.mainTexture = texture;
+        penSize = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // default values
+        whiteboardHover = false;
+        pressingMouse = false;
+    }
+
+    // draw
+    public Vector3 GetMouseWorldPosition()
+    {
+        //capture mouse position & return world point
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void OnMouseDown()
+    {
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("pressing mouse");
+        pressingMouse = true;
+    }
+
+    private void OnMouseOver()
+    {
+        whiteboardHover = true;
+        // Debug.Log("hovering over whiteboard");
+        if (pressingMouse)
+        {
+            Draw();
+        }
+    }
+
+    private void Draw()
+    {
+        texture.SetPixels((int) GetMouseWorldPosition().x, (int) GetMouseWorldPosition().y, blockWidth: penSize, blockHeight: penSize, pen_script.myColorArray);
+        texture.Apply();
+        Debug.Log("Drawing");
+    }
+}
